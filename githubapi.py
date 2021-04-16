de4f384469c94b5ae9c907ea2a696ff33a5baf9a
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2021-04-16 12:37:31 -0400

    adding code to find release note from github

---
+++ githubapi.py
@@ -0,0 +1,71 @@
+import os
+import requests
+import json
+token = os.environ['gh_token']
+headers = {'Authorization': 'token {}'.format(token)}
+
+def rest_call(url):
+    r = requests.get(url, headers=headers)
+    return json.loads(r.content)
+
+def run_query(query, variables): 
+    request = requests.post('https://api.github.com/graphql', 
+    json={'query': query, 'variables':variables}, headers=headers)
+    if request.status_code == 200:
+        return request.json()['data']
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(
+                request.status_code, query))
+
+
+def get_release_note(owner, name, version):
+    query = '''query ($repo_owner: String!, $repo_name: String!, $after: String){ 
+                    repository(owner: $repo_owner , name: $repo_name){
+                        releases(first:100, after: $after){
+                            totalCount
+                            nodes{
+                                id
+                                name
+                                publishedAt
+                                url
+                                tagCommit{
+                                    oid
+                                }
+                                tagName
+                            }
+                            pageInfo{
+                                hasNextPage
+                                endCursor
+                            }
+                        }
+                    }
+                }'''
+    variables = {
+            "repo_owner": owner,
+            "repo_name": name,
+            "after": None
+            }
+    
+    totalCount=None
+    releases = []
+    while True:
+        data=  run_query(query, variables)['repository']['releases']
+        totalCount=data['totalCount']
+        releases.extend(data['nodes'])
+        
+        for node in data['nodes']:
+            if node['name'].endswith(version):
+                return node
+
+        if data['pageInfo']['hasNextPage']:
+            variables["after"]=data['pageInfo']['endCursor']
+        else:
+            break
+
+    if len(releases)==totalCount:
+        return releases
+    else:
+        raise Exception('graphql call not functioning properly')
+
+if __name__=='__main__':
+    print(get_release_note('fastify','fastify','3.14.0'))
